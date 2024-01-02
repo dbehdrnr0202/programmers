34d83c5238414f17571ce9645e6169d760771825
dbehdrnr0202 <bb06001@naver.com>
2024-01-02 16:36:17 +0900

    git init

---
+++ KAKAO_BLIND_RECRUITMENT_2021/sol72416.py
@@ -0,0 +1,6 @@
+
+
+def solution(sales, links):
+    answer = 0
+    return answer
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from KAKAO_BLIND_RECRUITMENT_2021.sol72416 import solution
+
+
+
+if __name__=="__main__":
+    print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17], [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]))
