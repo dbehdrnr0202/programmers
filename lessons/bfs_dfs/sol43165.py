55619c09bf4fc9aef8a785b7225bbf142bc148d3
dbehdrnr0202 <bb06001@naver.com>
2024-01-19 14:29:13 +0900

    2024/01/19

---
+++ lessons/bfs_dfs/sol43165.py
@@ -0,0 +1,26 @@
+global g_number
+global g_target
+global g_answer
+
+def dfs(depth, cur_value):
+    global g_number
+    global g_target
+    global g_answer
+    if depth==len(g_number):
+        if cur_value==g_target:
+            g_answer+=1
+            return
+        return
+    dfs(depth+1, cur_value-g_number[depth])
+    dfs(depth+1, cur_value+g_number[depth])
+
+def solution(numbers, target):
+    answer = 0
+    global g_number
+    global g_target
+    global g_answer
+    g_answer = 0
+    g_number = numbers
+    g_target = target
+    dfs(0, 0)
+    return answer
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 #import KAKAO._2024_KAKAO_WINTER_INTERNSHIP.sol258711 as sol
 #import KAKAO._2018.sol17684 as sol
 #import practice.sol169198 as sol
-import lessons.sol85891 as sol
+import lessons.bfs_dfs.sol43165 as sol
 if __name__=="__main__":
-    quiz=["3 - 4 = -3", "5 + 6 = 11"]	
+    numbers = [4, 1, 2, 1]	
+    target = 4
     #[4, 6, 5, 1]
-    print(sol.solution(quiz))
+    print(sol.solution(numbers, target))
