4ff15177ed175d29ec376f599678140d86879883
dbehdrnr0202 <bb06001@naver.com>
2024-01-19 16:15:57 +0900

    2024/01/19

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 #import KAKAO._2024_KAKAO_WINTER_INTERNSHIP.sol258711 as sol
 #import KAKAO._2018.sol17684 as sol
 #import practice.sol169198 as sol
-import lessons.bfs_dfs.sol43165 as sol
+import lessons.bfs_dfs.sol43163 as sol
 if __name__=="__main__":
-    numbers = [4, 1, 2, 1]	
-    target = 4
+    begin = "hit"	
+    target = "cog"
+    words = ["hot", "dot", "dog", "lot", "log"]	
     #[4, 6, 5, 1]
-    print(sol.solution(numbers, target))
+    print(sol.solution(begin, target, words))
