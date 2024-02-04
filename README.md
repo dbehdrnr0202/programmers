2abcb096f5ec8cdba5f9c9c68b1934f6c5bc8a9b
dbehdrnr0202 <bb06001@naver.com>
2024-02-04 14:28:16 +0900

    2024/02/04

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # programmers
 programmers solution
+
+
+# N별 알고리즘 시간 복잡도
+|max(N)	    | Big O notation|
+|-----------|---------------|
+|500 	    |O(N³)          |
+|2,000 	    |O(N²)          |
+|100,000 	|O(NlogN)       |
+|10,000,000 |O(N)           |
---
+++ lessons/sol152996.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from itertools import combinations
+
+def solution(weights):
+    answer = 0
+    # counter를 써도된다
+    weight_dict = defaultdict(int)
+    for weight in weights:
+        weight_dict[weight]+=1
+    muls = [(2,3),(1,2),
+            (3,2),(3,4),
+            (2,1),(4,3)]
+    weight_values = weight_dict.keys()
+    for weight_key in weight_values:
+        if weight_dict[weight_key]>1:
+            answer+=(weight_dict[weight_key]-1)*weight_dict[weight_key]//2
+    for weight_value1, weight_value2 in combinations(weight_values, 2):
+        for mul in muls:
+            if mul[0]*weight_value1==mul[1]*weight_value2:
+                answer+=weight_dict[weight_value1]*weight_dict[weight_value2]
+    return answer
+
+weights = [100,180,360,100,270]	
+print(solution(weights))
