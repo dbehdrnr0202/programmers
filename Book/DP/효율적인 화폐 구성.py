c88e793cf4259ab53094114d85fc23dd89490c1e
dbehdrnr0202 <bb06001@naver.com>
2024-09-27 16:58:48 +0900

    2024/09/27

---
+++ Book/DP/효율적인 화폐 구성.py
@@ -0,0 +1,33 @@
+INF = 10000001
+
+def solution():
+    n, m = map(int, input().split())
+    lst = []
+    arr = [INF] * 10001
+    for _ in range(n):
+        money = int(input())
+        lst.append(money)
+        arr[money] = 1
+    for idx in range(1, m+1):
+        for money in lst:
+            arr[idx] = min(arr[idx-money]+1, arr[idx]) if (idx - money >= 0) and (arr[idx-money]!=INF) else arr[idx]
+    
+    return -1 if arr[m]==INF else arr[m]
+
+print(solution())
+
+'''
+CASE#1
+2 15
+2
+3
+
+5
+CASE#2
+3 4
+3
+5
+7
+
+-1
+'''
